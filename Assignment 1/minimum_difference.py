60defc5448f3bb5ddcf28f2629c12a5806e01376
ANBadawy <106503066+ANBadawy@users.noreply.github.com>
2022-07-16 07:25:16 +0200

    Add files via upload

---
+++ Assignment 1/minimum_difference.py
@@ -0,0 +1,33 @@
+"""With User Entering Numbers"""
+
+# def smallestDiff(list, n):
+#     list.sort()
+#     inf = 10 ** 5
+#     for i in range(n - 1):
+#         if list[i + 1] - list[i] < inf:
+#             inf = list[i + 1] - list[i]
+#     return inf
+#
+# n = int(input("Enter Number of list here "))
+# list = []
+# for j in range(n):
+#     element = int(input('Enter number '))
+#     list.append(element)
+#
+# print("Minimum difference is " + str(smallestDiff(list, n)))
+
+"""Without User Entering Numbers"""
+
+
+# def smallestDiff(list, n):
+#     list.sort()
+#     inf = 1000
+#     for i in range(n - 1):
+#         if list[i + 1] - list[i] < inf:
+#             inf = list[i + 1] - list[i]
+#     return inf
+#
+#
+# list = [1, 5, 3, 19, 18, 25]
+# n = len(list)
+# print("Minimum difference is " + str(smallestDiff(list, n)))
